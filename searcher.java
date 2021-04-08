abbeb3030fc8ba8ae473c190e93034d5d31ca5af
keykat <koin12000@naver.com>
2021-04-08 13:42:30 +0900

    ...

--- searcher.java
+++ searcher.java
@@ -105,13 +105,11 @@ public class searcher{
     public double[] InnerProduct(int documentSize, ArrayList<ArrayList<Double> > docWeight){
 
         double[] Qid = new double[documentSize + 1];
-
         for(int i = 1; i <= documentSize; i++){
             for(int j = 0; j < docWeight.get(i).size(); j+=2){
                 Qid[i] += docWeight.get(i).get(j) * docWeight.get(i).get(j + 1); 
             }
         }
-
         return Qid;
     }
 
